3ef3813f170012e1059d77ea0b91a59bcbf760d3
Thxzzzzz <july-7th@qq.com>
2019-06-03 20:21:10 +0800

    注释到日志logo

--- src/layout/header-aside/layout.vue
+++ src/layout/header-aside/layout.vue
@@ -1,74 +1,88 @@
 <template>
-  <div
-    class="d2-layout-header-aside-group"
-    :style="styleLayoutMainGroup"
-    :class="{grayMode: grayActive}">
+  <div class="d2-layout-header-aside-group"
+       :style="styleLayoutMainGroup"
+       :class="{grayMode: grayActive}">
     <!-- 半透明遮罩 -->
     <div class="d2-layout-header-aside-mask"></div>
     <!-- 主体内容 -->
-    <div class="d2-layout-header-aside-content" flex="dir:top">
+    <div class="d2-layout-header-aside-content"
+         flex="dir:top">
       <!-- 顶栏 -->
-      <div
-        class="d2-theme-header"
-        :style="{
+      <div class="d2-theme-header"
+           :style="{
           opacity: this.searchActive ? 0.5 : 1
         }"
-        flex-box="0"
-        flex>
-        <div class="logo-group" :style="{width: asideCollapse ? asideWidthCollapse : asideWidth}" flex-box="0">
-          <img v-if="asideCollapse" :src="`${$baseUrl}image/theme/${themeActiveSetting.name}/logo/icon-only.png`">
-          <img v-else :src="`${$baseUrl}image/theme/${themeActiveSetting.name}/logo/all.png`">
+           flex-box="0"
+           flex>
+        <div class="logo-group"
+             :style="{width: asideCollapse ? asideWidthCollapse : asideWidth}"
+             flex-box="0">
+          <img v-if="asideCollapse"
+               :src="`${$baseUrl}image/theme/${themeActiveSetting.name}/logo/icon-only.png`">
+          <img v-else
+               :src="`${$baseUrl}image/theme/${themeActiveSetting.name}/logo/all.png`">
         </div>
-        <div class="toggle-aside-btn" @click="handleToggleAside" flex-box="0">
-          <d2-icon name="bars"/>
+        <div class="toggle-aside-btn"
+             @click="handleToggleAside"
+             flex-box="0">
+          <d2-icon name="bars" />
         </div>
-        <d2-menu-header flex-box="1"/>
+        <d2-menu-header flex-box="1" />
         <!-- 顶栏右侧 -->
-        <div class="d2-header-right" flex-box="0">
+        <div class="d2-header-right"
+             flex-box="0">
           <!-- 如果你只想在开发环境显示这个按钮请添加 v-if="$env === 'development'" -->
-          <d2-header-search @click="handleSearchClick"/>
-          <d2-header-log/>
-          <d2-header-fullscreen/>
-          <d2-header-theme/>
-          <d2-header-size/>
-          <d2-header-user/>
+          <d2-header-search @click="handleSearchClick" />
+          <!-- <d2-header-log/>-->
+          <d2-header-fullscreen />
+          <d2-header-theme />
+          <d2-header-size />
+          <d2-header-user />
         </div>
       </div>
       <!-- 下面 主体 -->
-      <div class="d2-theme-container" flex-box="1" flex>
+      <div class="d2-theme-container"
+           flex-box="1"
+           flex>
         <!-- 主体 侧边栏 -->
-        <div
-          flex-box="0"
-          ref="aside"
-          class="d2-theme-container-aside"
-          :style="{
+        <div flex-box="0"
+             ref="aside"
+             class="d2-theme-container-aside"
+             :style="{
             width: asideCollapse ? asideWidthCollapse : asideWidth,
             opacity: this.searchActive ? 0.5 : 1
           }">
-          <d2-menu-side/>
+          <d2-menu-side />
         </div>
         <!-- 主体 -->
-        <div class="d2-theme-container-main" flex-box="1" flex>
+        <div class="d2-theme-container-main"
+             flex-box="1"
+             flex>
           <!-- 搜索 -->
           <transition name="fade-scale">
-            <div v-show="searchActive" class="d2-theme-container-main-layer" flex="dir:top">
-              <d2-panel-search
-                ref="panelSearch"
-                @close="searchPanelClose"/>
+            <div v-show="searchActive"
+                 class="d2-theme-container-main-layer"
+                 flex="dir:top">
+              <d2-panel-search ref="panelSearch"
+                               @close="searchPanelClose" />
             </div>
           </transition>
           <!-- 内容 -->
           <transition name="fade-scale">
-            <div v-show="!searchActive" class="d2-theme-container-main-layer" flex="dir:top">
+            <div v-show="!searchActive"
+                 class="d2-theme-container-main-layer"
+                 flex="dir:top">
               <!-- tab -->
-              <div class="d2-theme-container-main-header" flex-box="0">
-                <d2-tabs/>
+              <div class="d2-theme-container-main-header"
+                   flex-box="0">
+                <d2-tabs />
               </div>
               <!-- 页面 -->
-              <div class="d2-theme-container-main-body" flex-box="1">
+              <div class="d2-theme-container-main-body"
+                   flex-box="1">
                 <transition :name="transitionActive ? 'fade-transverse' : ''">
                   <keep-alive :include="keepAlive">
-                    <router-view/>
+                    <router-view />
                   </keep-alive>
                 </transition>
               </div>
@@ -89,7 +103,7 @@ import d2HeaderSearch from './components/header-search'
 import d2HeaderSize from './components/header-size'
 import d2HeaderTheme from './components/header-theme'
 import d2HeaderUser from './components/header-user'
-import d2HeaderLog from './components/header-log'
+// import d2HeaderLog from './components/header-log'
 import { mapState, mapGetters, mapActions } from 'vuex'
 import mixinSearch from './mixins/search'
 export default {
@@ -105,8 +119,8 @@ export default {
     d2HeaderSearch,
     d2HeaderSize,
     d2HeaderTheme,
-    d2HeaderUser,
-    d2HeaderLog
+    d2HeaderUser
+  //  d2HeaderLog
   },
   data () {
     return {
